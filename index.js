a596a29770d6d2ffe0ac5e7c91c46c54b7487619
Z4Tech <zhangchenguang1994@gmail.com>
2015-09-19 20:25:29 +0800

    bugFix

--- index.js
+++ index.js
@@ -10,9 +10,12 @@
 var bili_convert = require('bilibili-embed-convert');
 
 hexo.extend.tag.register('bilibili', function(args){
+  var av_id = parseInt(args[0]);
+  var page = parseInt(args[1]) || 1;
   var config = hexo.config.bilibili || {};
   config.width = config.width || 452;
   config.height = config.height || 544;
-  var bili_video = new bili_convert(args);
-  return bili_video.embedAddr();
+  var bili_video = new bili_convert(av_id, page);
+  console.log(bili_video.getIndex());
+  return bili_video.embedAddr(config.width, config.height);
 });
